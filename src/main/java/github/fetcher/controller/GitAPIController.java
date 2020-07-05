9786f9dadff69d82ae1e7907ecb6c24ee0973981
ammon-s <ammonsillah@gmail.com>
2020-07-05 18:48:58 +0200

    Adding Logger

--- src/main/java/github/fetcher/controller/GitAPIController.java
+++ src/main/java/github/fetcher/controller/GitAPIController.java
@@ -1,6 +1,8 @@
 package github.fetcher.controller;
 
 import github.fetcher.services.DataCollectionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/")
 public class GitAPIController {
+    final static Logger logger = LoggerFactory.getLogger(GitAPIController.class);
     private final DataCollectionService dataCollectorService;
 
     public GitAPIController(DataCollectionService dataCollectionService) {
@@ -19,7 +24,8 @@ public class GitAPIController {
 
     @RequestMapping(value = "/user/{username}",method= RequestMethod.GET,
             produces= MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUserData(@PathVariable("username")String username) {
+    public ResponseEntity<?> getUserData(@PathVariable("username")String username, HttpServletRequest request) {
+        logger.info(request.getRemoteAddr() + " requested userdate for: "+username);
         return dataCollectorService.getUserdata(username);
     }
 
--- src/main/java/github/fetcher/services/DataCollectionService.java
+++ src/main/java/github/fetcher/services/DataCollectionService.java
@@ -1,11 +1,15 @@
 package github.fetcher.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataCollectionService {
+
+    final static Logger logger = LoggerFactory.getLogger(DataCollectionService.class);
 
     public ResponseEntity<?> getUserdata(String username) {
         return new ResponseEntity<>("Initial", HttpStatus.OK);
